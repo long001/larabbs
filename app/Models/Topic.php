34b48068f9e5f6bf7bb85bf1f702d93f63912c36
long <396667187@qq.com>
2019-02-20 07:36:11 +0000

    Auto translate slug

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -44,4 +44,9 @@ class Topic extends Model
         return $query->orderBy('created_at', 'desc');
     }
 
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,10 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 //Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 //Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 //Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
