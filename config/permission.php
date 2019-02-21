d96d70ef2e02eac13d12bd03c3bbe5c2df3c1720
long <396667187@qq.com>
2019-02-21 02:30:13 +0000

    Integrated administrator

--- config/permission.php
+++ config/permission.php
@@ -1,129 +1,84 @@
 <?php
 
-return [
+return array(
 
-    'models' => [
+    // 后台的 URI 入口
+    'uri' => 'admin',
 
-        /*
-         * When using the "HasPermissions" trait from this package, we need to know which
-         * Eloquent model should be used to retrieve your permissions. Of course, it
-         * is often just the "Permission" model but you may use whatever you like.
-         *
-         * The model you want to use as a Permission model needs to implement the
-         * `Spatie\Permission\Contracts\Permission` contract.
-         */
+    // 后台专属域名，没有的话可以留空
+    'domain' => '',
 
-        'permission' => Spatie\Permission\Models\Permission::class,
+    // 应用名称，在页面标题和左上角站点名称处显示
+    'title' => env('APP_NAME', 'Laravel'),
 
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * Eloquent model should be used to retrieve your roles. Of course, it
-         * is often just the "Role" model but you may use whatever you like.
-         *
-         * The model you want to use as a Role model needs to implement the
-         * `Spatie\Permission\Contracts\Role` contract.
-         */
+    // 模型配置信息文件存放目录
+    'model_config_path' => config_path('administrator'),
 
-        'role' => Spatie\Permission\Models\Role::class,
+    // 配置信息文件存放目录
+    'settings_config_path' => config_path('administrator/settings'),
 
-    ],
-
-    'table_names' => [
-
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * table should be used to retrieve your roles. We have chosen a basic
-         * default value but you may easily change it to any table you like.
-         */
-
-        'roles' => 'roles',
-
-        /*
-         * When using the "HasPermissions" trait from this package, we need to know which
-         * table should be used to retrieve your permissions. We have chosen a basic
-         * default value but you may easily change it to any table you like.
-         */
-
-        'permissions' => 'permissions',
-
-        /*
-         * When using the "HasPermissions" trait from this package, we need to know which
-         * table should be used to retrieve your models permissions. We have chosen a
-         * basic default value but you may easily change it to any table you like.
-         */
-
-        'model_has_permissions' => 'model_has_permissions',
-
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * table should be used to retrieve your models roles. We have chosen a
-         * basic default value but you may easily change it to any table you like.
-         */
-
-        'model_has_roles' => 'model_has_roles',
-
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * table should be used to retrieve your roles permissions. We have chosen a
-         * basic default value but you may easily change it to any table you like.
-         */
-
-        'role_has_permissions' => 'role_has_permissions',
-    ],
-
-    'column_names' => [
-
-        /*
-         * Change this if you want to name the related model primary key other than
-         * `model_id`.
-         *
-         * For example, this would be nice if your primary keys are all UUIDs. In
-         * that case, name this `model_uuid`.
-         */
-
-        'model_morph_key' => 'model_id',
+    /*
+     * 后台菜单数组，多维数组渲染结果为多级嵌套菜单。
+     *
+     * 数组里的值有三种类型：
+     * 1. 字符串 —— 子菜单的入口，不可访问；
+     * 2. 模型配置文件 —— 访问 `model_config_path` 目录下的模型文件，如 `users` 访问的是 `users.php` 模型配置文件；
+     * 3. 配置信息 —— 必须使用前缀 `settings.`，对应 `settings_config_path` 目录下的文件，如：默认设置下，
+     *              `settings.site` 访问的是 `administrator/settings/site.php` 文件
+     * 4. 页面文件 —— 必须使用前缀 `page.`，如：`page.pages.analytics` 对应 `administrator/pages/analytics.php`
+     *               或者是 `administrator/pages/analytics.blade.php` ，两种后缀名皆可
+     *
+     * 示例：
+     *  [
+     *      'users',
+     *      'E-Commerce' => ['collections', 'products', 'product_images', 'orders'],
+     *      'Settings'  => ['settings.site', 'settings.ecommerce', 'settings.social'],
+     *      'Analytics' => ['E-Commerce' => 'page.pages.analytics'],
+     *  ]
+     */
+    'menu' => [
+        '用户与权限' => [
+            'users',
+        ],
     ],
 
     /*
-     * When set to true, the required permission/role names are added to the exception
-     * message. This could be considered an information leak in some contexts, so
-     * the default setting is false here for optimum safety.
+     * 权限控制的回调函数。
+     *
+     * 此回调函数需要返回 true 或 false ，用来检测当前用户是否有权限访问后台。
+     * `true` 为通过，`false` 会将页面重定向到 `login_path` 选项定义的 URL 中。
      */
+    'permission' => function () {
+        // 只要是能管理内容的用户，就允许访问后台
+        return Auth::check() && Auth::user()->can('manage_contents');
+    },
 
-    'display_permission_in_exception' => false,
+    /*
+     * 使用布尔值来设定是否使用后台主页面。
+     *
+     * 如值为 `true`，将使用 `dashboard_view` 定义的视图文件渲染页面；
+     * 如值为 `false`，将使用 `home_page` 定义的菜单条目来作为后台主页。
+     */
+    'use_dashboard' => false,
 
-    'cache' => [
+    // 设置后台主页视图文件，由 `use_dashboard` 选项决定
+    'dashboard_view' => '',
 
-        /*
-         * By default all permissions are cached for 24 hours to speed up performance.
-         * When permissions or roles are updated the cache is flushed automatically.
-         */
+    // 用来作为后台主页的菜单条目，由 `use_dashboard` 选项决定，菜单指的是 `menu` 选项
+    'home_page' => 'users',
 
-        'expiration_time' => \DateInterval::createFromDateString('24 hours'),
+    // 右上角『返回主站』按钮的链接
+    'back_to_site_path' => '/',
 
-        /*
-         * The key to use when tagging and prefixing entries in the cache.
-         */
+    // 当选项 `permission` 权限检测不通过时，会重定向用户到此处设置的路径
+    'login_path' => 'login',
 
-        'key' => 'spatie.permission.cache',
+    // 允许在登录成功后使用 Session::get('redirect') 将用户重定向到原本想要访问的后台页面
+    'login_redirect_key' => 'redirect',
 
-        /*
-         * When checking for a permission against a model by passing a Permission
-         * instance to the check, this key determines what attribute on the
-         * Permissions model is used to cache against.
-         *
-         * Ideally, this should match your preferred way of checking permissions, eg:
-         * `$user->can('view-posts')` would be 'name'.
-         */
+    // 控制模型数据列表页默认的显示条目
+    'global_rows_per_page' => 20,
 
-        'model_key' => 'name',
-
-        /*
-         * You may optionally indicate a specific cache driver to use for permission and
-         * role caching using any of the `store` drivers listed in the cache.php config
-         * file. Using 'default' here means to use the `default` set in cache.php.
-         */
-
-        'store' => 'default',
-    ],
-];
+    // 可选的语言，如果不为空，将会在页面顶部显示『选择语言』按钮
+    'locales' => [],
+);
