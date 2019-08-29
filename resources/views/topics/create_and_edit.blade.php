37435c63e1b7616507887acc0f26c9acc5e7c2f8
long <396667187@qq.com>
2019-08-29 16:01:04 +0800

    配置 文本编辑器 simditor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -78,6 +78,7 @@
                     leaveConfirm: '文件上传中，关闭此页面将取消上传。'
                 },
                 pasteImage: true,
+		toolbar:  ['title', 'bold', 'italic', 'underline', 'strikethrough', 'fontScale', 'color', 'ol', 'ul', 'blockquote', 'code', 'table', 'link', 'image', 'hr', 'indent', 'outdent', 'alignment'], //工具条都包含哪些内容
             });
         });
     </script>
